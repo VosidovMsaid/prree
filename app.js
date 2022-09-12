fba196234e98e83f3a22870c6aed2b195e4863ae
VosidovMsaid <93457968+VosidovMsaid@users.noreply.github.com>
2022-09-12 18:07:31 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log("Hello world");
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
